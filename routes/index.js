e378fc05c99d99f4da0e7d42471e3ad374fb6bd7
“AdejokeOgunyinka” <ogunyinkajoke@yahoo.com>
2021-01-30 18:16:13 +0100

    Adjusted index page view.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('GET Method does not exist. Add /api/rates to your url path.')
 });
 
 module.exports = router;
